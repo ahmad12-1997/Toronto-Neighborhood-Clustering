9a9077b1b65fbb303fb9eae135a535eeadf9ef71
ahmad12-1997 <67412352+ahmad12-1997@users.noreply.github.com>
2021-06-06 18:56:51 +0300

    Add files via upload

---
+++ Toronto Segmentation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>This Note book will be used for the Data Science capstone</h1>
+
+# In[1]:
+
+
+import pandas as pd 
+import numpy as np
+
+
+# In[2]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
